ae157cb3e4e106f043993792d2188452dc3b50b8
daniilzverev <viktormolot82@gmail.com>
2024-03-03 14:54:59 +0100

    feat(DbContextStudentTest): added Test of add student

--- src/StudIS.DAL.Tests/DbContextStudentTests.cs
+++ src/StudIS.DAL.Tests/DbContextStudentTests.cs
@@ -1,12 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using StudIS.DAL.Entities;
 using Xunit.Abstractions;
 
 namespace StudIS.DAL.Tests;
 
 public class DbContextStudentTests(ITestOutputHelper output) : DbContextTestsBase(output)
 {
-    // [Fact]
-    // public async Task Test1()
-    // {
-    //     await StudIsDbContextSUT.SaveChangesAsync();
-    // }
-} 
+    [Fact]
+    public async Task Test1()
+    {
+        StudentEntity studentId = new(
+            Guid.NewGuid(),
+            "Nikita",
+            "http://example.com/photo.jpg");
+        
+        
+        StudIsDbContextSUT.Students.Add(studentId);
+        await StudIsDbContextSUT.SaveChangesAsync();
+
+        await using StudIsDbContext dbxContext = await DbContextFactory.CreateDbContextAsync();
+        StudentEntity actualdbxContext= await dbxContext.Students.SingleAsync(i => i.Id == studentId.Id);
+        Assert.Equal(studentId,actualdbxContext);
+        
+    }
+}
