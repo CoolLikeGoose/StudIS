57c75e34dcbb502ee92c20e2100b2f9b226040b2
CoolLikeGoose <niken54@mail.ru>
2024-04-05 05:30:17 +0200

    feat(BL): added base facade and model

---
+++ src/StudIS.BL/Facades/IFacade.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using StudIS.DAL.Entities;
+
+namespace StudIS.BL.Facades;
+
+public interface IFacade<TEntity, TListModel, TDetailModel>
+    where TEntity : class, IEntity
+    where TDetailModel : class
+{
+    Task DeleteAsync(Guid id);
+    Task<TDetailModel?> GetAsync(Guid id);
+    Task<IEnumerable<TListModel>> GetAsync();
+    Task<TDetailModel> SaveAsync(TDetailModel model);
+}
