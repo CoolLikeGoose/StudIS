8e86c09166023be2e28813cb34625bbf830b13ff
CoolLikeGoose <niken54@mail.ru>
2024-04-07 15:48:43 +0200

    fix(DAL/tests): fixed subject delete

--- src/StudIS.DAL.Tests/DbContextSubjectTests.cs
+++ src/StudIS.DAL.Tests/DbContextSubjectTests.cs
@@ -59,18 +59,12 @@ public class DbContextSubjectTests(ITestOutputHelper output) : DbContextTestsBas
     [Fact]
     public async Task Delete_Subject()
     {
-        // Arrange
-        var subject = SubjectSeeds.BasicSubject;
-        StudIsDbContextSUT.Subjects.Add(subject);
-        await StudIsDbContextSUT.SaveChangesAsync();
-
-        // Act
-        StudIsDbContextSUT.Subjects.Remove(subject);
+        StudIsDbContextSUT.Subjects.Remove(SubjectSeeds.StandardInDbSubject);
         await StudIsDbContextSUT.SaveChangesAsync();
 
         // Assert
         await using var dbContext = await DbContextFactory.CreateDbContextAsync();
-        var deletedSubject = await dbContext.Subjects.FindAsync(subject.Id);
+        var deletedSubject = await dbContext.Subjects.FindAsync(SubjectSeeds.StandardInDbSubject.Id);
         Assert.Null(deletedSubject);
     }
     
