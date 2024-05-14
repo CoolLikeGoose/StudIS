17e59b57003d1588b6038475cfd753bef1bbe947
CoolLikeGoose <niken54@mail.ru>
2024-05-14 14:29:57 +0200

    feat: Added main page layout

--- src/StudIS.APP/MainPage.xaml.cs
+++ src/StudIS.APP/MainPage.xaml.cs
@@ -2,22 +2,27 @@
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
+    // int count = 0;
 
     public MainPage()
     {
         InitializeComponent();
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
+    // private void OnCounterClicked(object sender, EventArgs e)
+    // {
+    //     count++;
+    //
+    //     if (count == 1)
+    //         CounterBtn.Text = $"Clicked {count} time";
+    //     else
+    //         CounterBtn.Text = $"Clicked {count} times";
+    //
+    //     SemanticScreenReader.Announce(CounterBtn.Text);
+    // }
+
+    private void OnSearchClicked(object? sender, EventArgs e)
     {
-        count++;
-
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
+        throw new NotImplementedException();
     }
 }
---
+++ src/StudIS.APP/Pages/StudentsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudIS.APP;
+
+public partial class StudentsPage : ContentPage
+{
+    public StudentsPage()
+    {
+        InitializeComponent();
+    }
+}
