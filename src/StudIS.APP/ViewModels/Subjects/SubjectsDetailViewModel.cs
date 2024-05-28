4abc3355b254cc4f57c950bdb561b2f6cdb0a435
vVladyslaVv <vladyslav.yeroma@gmail.com>
2024-05-28 19:42:59 +0200

    feat: Added CRUD Operations

--- src/StudIS.APP/ViewModels/Subjects/SubjectsDetailViewModel.cs
+++ src/StudIS.APP/ViewModels/Subjects/SubjectsDetailViewModel.cs
@@ -1,20 +1,32 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using StudIS.BL.Facades.Interfaces;
 using StudIS.BL.Models;
 
 namespace StudIS.APP.ViewModels.Subjects;
 
-public partial class SubjectsDetailViewModel(ISubjectFacade subjectFacade) : IViewModel, IQueryAttributable, INotifyPropertyChanged
+public partial class SubjectsDetailViewModel : ObservableObject, IViewModel, IQueryAttributable, INotifyPropertyChanged
 {
+    private readonly ISubjectFacade _subjectFacade;
     private Guid Id { get; set; }
 
-    public SubjectDetailModel? Subject { get; set; }
+    public SubjectsDetailViewModel(ISubjectFacade subjectFacade)
+    {
+        _subjectFacade = subjectFacade;
+    }
+
+    private SubjectDetailModel _subject;
+    public SubjectDetailModel Subject
+    {
+        get => _subject;
+        set => SetProperty(ref _subject, value);
+    }
 
     public async Task LoadDataAsync()
     {
-        Subject = await subjectFacade.GetAsync(Id);
+        Subject = await _subjectFacade.GetAsync(Id);
         OnPropertyChanged("Subject");
     }
 
@@ -22,6 +34,19 @@ public partial class SubjectsDetailViewModel(ISubjectFacade subjectFacade) : IVi
     {
         Id = (Guid)query["Id"];
         await LoadDataAsync();
+    }
+
+    [RelayCommand]
+    private async Task DeleteAsync()
+    {
+        await _subjectFacade.DeleteAsync(Id);
+        await Shell.Current.GoToAsync("..");
+    }
+
+    [RelayCommand]
+    private async Task EditAsync()
+    {
+        await Shell.Current.GoToAsync("edit", new Dictionary<string, object> { { "Id", Id } });
     }
 
     public event PropertyChangedEventHandler? PropertyChanged;
--- src/StudIS.APP/ViewModels/Subjects/SubjectsEditViewModel.cs
+++ src/StudIS.APP/ViewModels/Subjects/SubjectsEditViewModel.cs
@@ -1,11 +1,64 @@
-﻿using StudIS.BL.Facades.Interfaces;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using StudIS.BL.Facades.Interfaces;
+using StudIS.BL.Models;
 
 namespace StudIS.APP.ViewModels.Subjects;
 
-public class SubjectsEditViewModel(ISubjectFacade subjectFacade) : IViewModel
+public partial class SubjectsEditViewModel : ObservableObject, IViewModel, IQueryAttributable
 {
-    public Task LoadDataAsync()
+    private readonly ISubjectFacade _subjectFacade;
+    private Guid _subjectId;
+
+    public SubjectsEditViewModel(ISubjectFacade subjectFacade)
     {
-        return Task.CompletedTask;
+        _subjectFacade = subjectFacade;
+    }
+
+    private SubjectDetailModel _subject;
+    public SubjectDetailModel Subject
+    {
+        get => _subject;
+        set => SetProperty(ref _subject, value);
+    }
+
+    public async Task LoadDataAsync()
+    {
+        if (_subjectId != Guid.Empty)
+        {
+            Subject = await _subjectFacade.GetAsync(_subjectId);
+        }
+        else
+        {
+            Subject = new SubjectDetailModel
+            {
+                Name = "Default Name", // Default value for Name
+                Abbreviation = "DEF"  // Default value for Abbreviation
+            };
+        }
+    }
+
+    public async void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        if (query.ContainsKey("Id"))
+        {
+            _subjectId = (Guid)query["Id"];
+        }
+        await LoadDataAsync();
+    }
+
+    [RelayCommand]
+    private async Task SaveAsync()
+    {
+        await _subjectFacade.SaveAsync(Subject);
+        await Shell.Current.GoToAsync("..");
+    }
+
+    [RelayCommand]
+    private async Task CancelAsync()
+    {
+        await Shell.Current.GoToAsync("..");
     }
 }
